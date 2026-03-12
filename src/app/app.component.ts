646e042f07d384c96889d0c8958a2f50b0853db7
tuananh0737 <rox.tuananh@gmail.com>
2026-03-12 14:02:52 +0700

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { WeatherService } from './services.service';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  standalone: true, 
+  imports: [FormsModule, CommonModule], 
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export const environment = {
-  production: false,
-  openWeatherApiKey: 'YOUR_OPENWEATHERMAP_API_KEY' 
-};
+export class AppComponent implements OnInit {
+  city: string = 'Hanoi'; 
+  weatherData: any;
+  errorMessage: string = '';
 
+  constructor(private weatherService: WeatherService) {}
+
+  ngOnInit() {
+    this.searchWeather(); 
+  }
+
+  searchWeather() {
+    if (!this.city.trim()) return;
+    
+    this.weatherService.getCurrentWeather(this.city).subscribe({
+      next: (data) => {
+        this.weatherData = data;
+        this.errorMessage = ''; 
+      },
+      error: (err) => {
+        this.errorMessage = 'Không tìm thấy thông tin thành phố này hoặc có lỗi API!';
+        this.weatherData = null; 
+        console.error(err);
+      }
+    });
+  }
+}
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,8 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true })]
+  providers: [
+    provideHttpClient()
+  ]
 };
